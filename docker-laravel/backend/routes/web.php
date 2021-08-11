b8833d5a5089df564dc66e0f0d8d31ebcc60ed73
henmihenmi <kentaro4413@gmail.com>
2021-08-11 21:52:42 +0900

    回答機能のモデル・テーブルを作成

--- docker-laravel/backend/routes/web.php
+++ docker-laravel/backend/routes/web.php
@@ -13,3 +13,4 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/', [QuestionController::class, 'index'])->name('questions.index');
 Route::resource('questions', QuestionController::class)->only(['show', 'create','store']);
+Route::resource('answers', AnswerController::class);
