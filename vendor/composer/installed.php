12774f719a939be22d7b7acbf30a3a7cd6152133
henmihenmi <kentaro4413@gmail.com>
2021-08-29 18:42:30 +0900

    composer updateを実施

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '977cdf3cfda4c1e6041b68b502df931b1dc17325',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '977cdf3cfda4c1e6041b68b502df931b1dc17325',
+            'dev_requirement' => false,
+        ),
+    ),
+);
